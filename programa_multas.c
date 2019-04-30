592863d737bcd2cd39ee7d2e8153bc8dc5b32cb6
jorgedemiguele <jorge.demiguele@alumnos.upm.es>
2019-04-30 18:37:04 +0200

    inicializacion del la rama del alcohol

--- programa_multas.c
+++ programa_multas.c
@@ -25,14 +25,18 @@ typedef struct {
 // prototipos
 void CargaRadaresManual();
 void CargaMultasManual();
-void RellenarUnRadar();
-void RellenarUnaMulta();
+void RellenarUnRadar(T_RADAR *prad);
+void RellenarUnaMulta(T_MULTA *pmul);
+void RellenarFecha(T_FECHA *pfecha);
 
 int main(){
     
     int op;
     int n_multas;
     int n_radares;
+    char dni[10];
+    float tasa;
+    char p;
     
 	//En caso de no elegir ni la primera ni la segunda vuelve a preguntar 
 	do{
@@ -45,8 +49,9 @@ int main(){
 	
     switch(op){
 		case 1:
-			printf("funcion primer programa\n");
+			printf("\nFuncion primer programa");
             
+            //pide el numero de radares y se repite hasta que sea un numero positivo
             do{
                printf("\nIntroduce el numero de radares: ");
                scanf("%d",&n_radares);
@@ -54,6 +59,8 @@ int main(){
                    printf("\nDebe ser positivo");
                }
            }while(n_radares<=0);
+           
+           //pide el numero de ultas y se repite hasta que el numero sea posivo 
            do{
                printf("\nIntroduce el numero de multas: ");
                scanf("%d",&n_multas);
@@ -67,8 +74,44 @@ int main(){
 			break;
             
 		case 2:
-			printf("funcion segundo programa\n");
+			printf("\nFuncion segundo programa");
+            
+            //pide los datos del conductor
+            printf("\nIntroduce el DNI: ");
+            scanf("%9s", dni);
+            RellenarFecha
+            printf("\nIntroduce la tasa de alcohol del conductor: ");
+            scanf("%f", &tasa);
+            //dependiendo de si el conductor es noble o no se le apican inas tasas de medicion u otras
+            printf("\n¿Es noble o el conductor?: si o no");
+            switch(p){
+                case 'si':
+                if(tasa<0.15){
+                    printf("\nNo tiene sancion administrativa");
+                }else if(tasa>0.15 && tasa<=0.30){
+                    printf("\nMulta de 500 euros");
+                }else if(tasa>0.3 && tasa<=0.6){
+                    printf("\nMulta de 1000 euros");
+                }
+                break;
+                
+                case 'no':
+                if(tasa<0.25){
+                    printf("\nNo tiene sancion administrativa");
+                }else if(tasa>0.25 && tasa<=0.5){
+                    printf("\nMulta de 500 euros");
+                }else if(tasa>0.5 && tasa<=0.6){
+                    printf("\nMulta de 1000 euros");
+                }
+                break;
+                
+                default:
+                printf("\nError");
+            }
         break;
+        
+        default:
+        printf("\nError");
     }
    
    return 0;
@@ -91,8 +134,8 @@ void CargaMultasManual(){
     return;
 }
 
-
-void RellenarUnRadar(){
+//para introducir todos los datos q componen el rardar
+void RellenarUnRadar(T_RADAR *prad){
     printf("\nIntroduce el identificador del radar: ");
     scanf("%d",&(prad->id_radar));
     printf("\nIntroduce la velocidad limite: ");
@@ -106,14 +149,26 @@ void RellenarUnRadar(){
     return;
 }
 
-
-void RellenarUnaMulta(){
-    RellenarFecha();
+//introduce todos los datos q cumponen la multa
+void RellenarUnaMulta(T_MULTA *pmul){
+    RellenarFecha(&fecha);
     printf("\nIntroduce el numero identificador del radar: ");
     scanf("%d", &(pmul->id_radar));
     printf("\nIntroduce la matricula: ");
-    //scanf("", );
+    scanf("%Ns", &(pmul->matricula));
     printf("\nIntroduce la velocidad: ");
     scanf("%d", &(pmul->velocidad));
+    return;
+}
+
+
+void RellenarFecha(T_FECHA *pfecha){
+    printf("\nIntroduce la fecha:");
+    printf("\nDia: ");
+    scanf("%d", &(pfecha->dia));
+    printf("\nMes: ");
+    scanf("%d", &(pfecha->mes));
+    printf("\nAnio: ");
+    scanf("%d", &(pfecha->anio));
     return;
 }
