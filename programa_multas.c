6fe0b9147fe0d5c28a650d28a29fea41db0d51d0
Alberto1166 <a.dbejerano@alumnos.upm.es>
2019-05-16 18:23:20 +0200

    prueba comprobante dni

--- programa_multas.c
+++ programa_multas.c
@@ -250,9 +250,33 @@ void RellenarMultaAlcohol(T_ALCOHOL *palc){
     T_FECHA fecha;
     RellenarFecha(&fecha);
     char res;
-    printf("\nIntroduce el dni(Con letra): ");
-    scanf(" %s", &(palc->dni));
-
+    
+    printf("\nIntroduce el dni(Con letra): ");{
+    scanf(" %s", &(palc->dni)); //Creo que tengo cambiar el (palc->dni) por el (strlen(dni)
+	if(strlen(dni)<9) {
+		return "DNI demasiado corto.";
+	}
+	
+	dni = strtoupper(dni);
+ 
+	letra = substr(dni, -1, 1);
+	numero = substr(dni, 0, 8);
+		
+	// Si es un NIE hay que cambiar la primera letra por 0, 1 ó 2 dependiendo de si es X, Y o Z.
+	numero = str_replace(array('X', 'Y', 'Z'), array(0, 1, 2), numero);	
+ 
+	modulo = numero % 23;
+	letras_validas = "TRWAGMYFPDXBNJZSQVHLCKE";
+	letra_correcta = substr(letras_validas, modulo, 1);
+	
+	if(letra_correcta!=letra) { // Despues de saber que la letra es incorrecta no quiero que me diga cual es la letra 
+	 							//sino que me diga que ese dni no existe y que me pida introducirle de nuevo
+		return "Letra incorrecta, la letra deber&iacute;a ser la $letra_correcta."; //
+	}
+	else {
+		return "OK";
+	}
+}
     printf("\nIntroduce la tasa de alcohol del conductor: ");
     scanf(" %f", &(palc->tasa));
 
